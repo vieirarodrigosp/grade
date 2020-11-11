50bd5a11708fd0aeb8b9633ddb30bf22eece6b57
Rodrigo Vieira Barbosa <rv_rodrigo.vieira@hotmail.com>
2020-11-11 07:59:05 -0300

    Inicio projeto de school, API grade.

---
+++ src/test/java/vieirabarbosa/com/br/grade/GradeApplicationTests.java
@@ -0,0 +1,13 @@
+package vieirabarbosa.com.br.grade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
